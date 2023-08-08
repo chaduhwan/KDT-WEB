22454189dd96f46471f9be33db7b5945d36bfc8c
chaduhwan <enghks2973@naver.com>
2023-08-08 16:36:09 +0900

    prac : mvc_mysql

--- 14_mvc_mysql/controller/Cvisitor.js
+++ 14_mvc_mysql/controller/Cvisitor.js
@@ -4,6 +4,32 @@ exports.main = (req,res) => {
     res.render(`index`)
 }
 
-exports.getVisitor= (req,res) => {
-    res.render(`visitor`,{data : Visitor.getVisitors()});
+exports.getVisitors= (req,res)=>{
+ Visitor.getvisitors((rows)=>{
+    res.render(`visitor`,{data : rows});
+ });
+}
+
+exports.getVisitor =(req,res)=>{
+  Visitor.getvisitor(req.body.id,(result)=>{
+    res.render(`visitor`,{data : result});
+  })
+}
+
+exports.postVisitor =(req,res)=>{
+  Visitor.postVisitor(req.body,(result)=>{
+    res.send({id:result.insertId,name :req.body.name, comment :req.body.comment})
+  })
+}
+
+exports.patchVisitor=(req,res)=>{
+    Visitor.patchVisitor(req.body,()=>{
+        res.send({result : true});
+    })
+}
+
+exports.deleteVisitor =(req,res)=>{
+    Visitor.deleteVisitor(req.body,()=>{
+        res.send({result : true});
+    })
 }
--- 14_mvc_mysql/index.js
+++ 14_mvc_mysql/index.js
@@ -1,7 +1,6 @@
 const express = require(`express`);
 const app = express();
 const PORT = 8000;
-const mysql =require(`mysql`);
 
 
 app.set("view engine",`ejs`);
@@ -10,65 +9,17 @@ app.set(`views`,`./views`);
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
-// mysql 연결
-const conn = mysql.createConnection({
-    host:'127.0.0.1', // 맥북은 localhost 대신 127.0.0.1
-    user :'hwandu',
-    password : 'asd123',
-    database : 'kdt9',
-    port : 3306
-});
-
-conn.connect((err)=>{
-    if(err) {
-        console.log(err)
-        return;
-    }
-    console.log(`connect`);
-})
-
+//router
 // const router = require(`./routes`);
 // app.use(`/`,router);
+const visitorRouter = require(`./routes/visitor`)
+
 app.get(`/`,(req,res)=>{
     res.render(`index`);
 })
 
-// 방명록 전체 보기
-app.get(`/visitor`,(req,res)=>{
-    const query = `select * from visitors`
-    conn.query(query,(err,rows)=>{
-        console.log(rows);
-        res.render(`visitor`,{data : rows});
-    });
-});
-
-// 방명록 하나 조회
-app.get(`/visitor/get`,(req,res)=>{
-    res.send(`하나조회`)
-})
-
-// 방명록 하나 생성
-app.post(`/visitor/write`,(req,res)=>{
-    const query = `insert into visitors (name,comment) values ('${req.body.name}','${req.body.comment}')`
-    conn.query(query,(err,rows)=>{
-        console.log(`rows`,rows)
-    res.send({id:rows.insertId,name :req.body.name, comment :req.body.comment})
-    })
-    
-})
-
-// 방명록 하나 수정
-app.patch(`/visitor/edit`,(req,res)=>{
-    res.send(`하나 수정`)
-})
-
-// 방명록 하나 삭제
-app.delete(`/visitor/delete`,(req,res)=>{
-    res.send(`하나 삭제`)
-})
-
-
-
+//localhost:8000/visitor ~ 로 시작하는 
+app.use('/visitor',visitorRouter)
 
 app.use(`*`,(req,res)=>{
     res.render(`404`)
