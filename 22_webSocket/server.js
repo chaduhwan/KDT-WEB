158950c7601402fc5a3c36afc21c8480e2b648da
chaduhwan <enghks2973@naver.com>
2023-08-28 17:10:44 +0900

    prac : web socket

---
+++ 22_webSocket/server.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const app = express();
+const ws = require('ws');
+const PORT = 8000;
+
+app.set('view engine','ejs')
+
+app.get('/',(req,res)=>{
+    res.render('client');
+});
+
+const server = app.listen(PORT,()=>{
+    console.log(`http://localhost${PORT}`)
+})
+
+//app.listen 밑에 소켓은 있어도 됨.
+// 웹소켓 서버 접속
+const wss = new ws.Server({server}); // server : server
+
+//브라우저(클라이언트)들을 담을 배열변수
+const sockets = [];
+
+//socket변수는 접속한 브라우저를 가리킴.
+wss.on('connection',(socket)=>{
+    console.log(socket);
+    //sockets 배열에 브라우저 정보 추가
+    sockets.push(socket);
+    
+    console.log('클라이언트가 연결되었습니다.')
+
+    //메세지 이벤트
+    socket.on('message',(message)=>{
+        console.log(`클라이언트로 받은 메세지 ${message}`)
+        //socket.send(`서버메세지:${message}`)
+        sockets.forEach(elem=>{
+            elem.send(`전체 메세지:${message}`)
+        })
+    })
+
+    //오류 이벤트
+    socket.on('error',(error)=>{
+        console.log('에러발생',error)
+    })
+
+    //접속 종류 이벤트
+    socket.on('close',()=>{
+        console.log('클라이언트와 연결이 종료되었습니다.')
+    })
+})
